e5d75b60bc92fdf53280bb7b4a7e65be157b41f8
mibrgmv <mibrgmv@gmail.com>
2024-09-27 04:55:36 +0300

    feat: js for todo list – add, mark and delete tasks

---
+++ todo.js
@@ -0,0 +1,12 @@
+// const inputBox = document.getElementById("input-box")
+// const listContainer = document.getElementById("task-list")
+//
+// function addTask() {
+//     if (inputBox.value === '') {
+//         alert("DUrAK")
+//     } else {
+//         let li = document.createElement("li");
+//         li.innerHTML = inputBox.value;
+//         listContainer.appendChild(li);
+//     }
+// }
